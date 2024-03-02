0d79132eea635271d768e51118791776d9b473b6
vivdto@gmail.com <vivdto@gmail.com>
2024-03-02 16:08:29 +0530

    AssignmentOp.Cpp

---
+++ Operators/AssignmentOp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 10, y; // x, y are the varibles; 
+    y = x; 
+    cout << y << '\n';
+    x += 10;
+    cout << x << '\n';
+    x -= 10;
+    cout << x << '\n';
+    x *= 2;
+    cout << x << '\n';
+    return 0;
+}
