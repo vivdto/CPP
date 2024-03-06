699c4fe43cd8a38e9e3ea2f1b6aab11dc068bef8
vivdto@gmail.com <vivdto@gmail.com>
2024-03-06 22:01:03 +0530

    Array Programs

---
+++ Array/ArrayTraversal.cpp
@@ -0,0 +1,13 @@
+void arrayTraversal(int numbers[], int size){
+    //Write your code below
+    
+    
+    
+    for (int i = 0; i<size; i++)
+    {
+        cout<<numbers[i]<<" ";
+    }
+    
+    
+    //Don't provide the new line
+}
---
+++ Array/decrementArrayElements.cpp
@@ -0,0 +1,13 @@
+int * decrementArrayElements(int numbers[], int size){
+    
+    //write your code here
+    
+    
+    for (int i = 0; i<size; i++)
+    {
+
+	numbers[i] = numbers[i] - 1;
+
+    }
+return numbers;    
+}
