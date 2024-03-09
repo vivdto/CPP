210bbf564c6f66beccee83fcceb7c9575d1dac14
vivdto@gmail.com <vivdto@gmail.com>
2024-03-10 01:10:52 +0530

    Pointer Programs.

---
+++ Pointers/dereferencing.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+
+int x = 5;
+int *y = &x;
+cout<<*y; // Derefrencing the y pointer
+
+//two ways to refer to the same position
+
+
+
+return 0;
+}
---
+++ Pointers/swap.cpp
@@ -0,0 +1,10 @@
+/*
+
+Dynamic Memory --> Pointer
+New keyword is dynamic memory in c++; it returns a pointer.
+
+
+
+new keyword --> Head Memory; 
+*/
+
