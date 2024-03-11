ac12dfcdcd081f79122131fd2e1cc3b68af4ca63
vivdto@gmail.com <vivdto@gmail.com>
2024-03-11 12:49:16 +0530

    Added Pointer Questions

---
+++ Pointers/ambiguous.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void fun(int x){
+    
+}
+
+void fun(int *ptr)
+{
+    
+}
+
+//main.cpp:17:5: error: call of overloaded ‘func(NULL)’ is ambiguous
+
+
+
+int main ()
+{
+    
+fun(NULL);
+return 0;
+
+}
