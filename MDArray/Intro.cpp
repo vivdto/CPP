a0ae934d6850cdad06efabf76246c6cd5afdba71
vivdto@gmail.com <vivdto@gmail.com>
2024-03-14 12:04:39 +0530

    Added new programs

---
+++ MDArray/Intro.cpp
@@ -0,0 +1,52 @@
+/* Embark on a journey into the realm of matrices in data structures and algorithms, where their elegant manipulation and efficient operations will revolutionize your problem-solving skills and elevate your programming prowess. In this section we will discuss: Introduction to Multidimensional Array in C++
+
+A multi-dimensional array can be termed as an array of arrays that stores homogeneous data in tabular form. Data in multidimensional arrays is generally stored in row-major order in the memory.
+
+
+
+Example:                                              2D Array
+
+C++ Return 2D Array From Function - GeeksforGeeks
+
+https://media.geeksforgeeks.org/wp-content/uploads/20221201105954/2DArray.png
+
+3D Array
+
+graphical representation of three dimensional array
+
+https://media.geeksforgeeks.org/wp-content/uploads/3D-array.jpg
+
+In this video, we will discuss the following aspects:
+
+The general form of declaring N-dimensional arrays
+Size of Multidimensional Arrays
+Important Points Related to Multidimensional Arrays
+The most commonly used forms of the multidimensional array
+2D Array
+Initialization of Two-Dimensional Arrays 
+Accessing Elements of Two-Dimensional Arrays 
+Ways of Creating  Multidimensional Arrays in C++
+Double Pointer Method
+Array of Pointers
+Array of Vectors
+Vector of Vectors
+3D Array
+
+
+Codes:
+
+Example 6(Vector of Vectors)
+CPP: https://ide.geeksforgeeks.org/SvjKglUvpp
+Example 5(Array of Vectors)
+CPP: https://ide.geeksforgeeks.org/DsGReyLzi2
+Example 4(Array of Pointer)
+CPP: https://ide.geeksforgeeks.org/acygPwZSHN
+Example 3(Double Pointer)
+CPP: https://ide.geeksforgeeks.org/vVVUMw7RA4
+Example 2(Variable Sized Arrays)
+CPP: https://ide.geeksforgeeks.org/GAIRrkOp8Y
+Example 1(Fixed Sized Arrays)
+CPP: https://ide.geeksforgeeks.org/d2D7EeGkMh
+
+*/
+
